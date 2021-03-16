89fc7f82f5b2a00d0014deaebd44aee9130fa499
MrGithub2021 <77593158+MrGithub2021@users.noreply.github.com>
2021-03-16 09:01:00 -0400

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -1,6 +1,11 @@
-def test_true():
-    assert True
-
-
-def test_false():
-    assert False is False
+print('Can you read this?')
+for x in "Bananas":
+  print(x) 
+for y in "Are":
+  print(y)
+for z in 'Awesome':
+  print(z)
+print('If so, than congrats!')
+for a in 'Applesaretasty!':
+  print(a)
+print('Try to Figure out the secret message!')  
